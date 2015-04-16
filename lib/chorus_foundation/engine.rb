3b39329f02b964b4f1538c1abd5e9f7013b6de48
prakash-alpine <prakash@alpinenow.com>
2015-04-16 10:47:20 -0700

    Adding bootstrap project management template and style documentation files.

--- lib/chorus_foundation/engine.rb
+++ lib/chorus_foundation/engine.rb
@@ -1,4 +1,10 @@
 module ChorusFoundation
   class Engine < ::Rails::Engine
+
+    # Initializer to combine this engines static assets with the static assets of the hosting site.
+    initializer 'static assets' do |app|
+      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+    end
+
   end
 end
